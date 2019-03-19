4992bda8b9d6ab32e2ab4c8f133fc60076f53bff
Rogelio Consejo <rogelio.consejo@gmail.com>
2019-03-19 12:49:01 -0600

    Implementado decenas. Funciona hasta el Noventa y Nueve.

--- src/com/auebo/utils/cantidadestoletras/CantidadesToLetras.java
+++ src/com/auebo/utils/cantidadestoletras/CantidadesToLetras.java
@@ -1,6 +1,57 @@
 package com.auebo.utils.cantidadestoletras;
 
 public class CantidadesToLetras {
+    private enum Decenas{
+        Cero(0),
+        Diez(1),
+        Veinte(2),
+        Treinta(3),
+        Cuarenta(4),
+        Cincuenta(5),
+        Sesenta(6),
+        Setenta(7),
+        Ochenta(8),
+        Noventa(9);
+
+        public final int valor;
+
+        Decenas(int valor) {
+            this.valor = valor;
+        }
+
+        /**
+         * Retorna el nombre con letras de una unidad.
+         * @param digito    representa una cantidad de unidades
+         * @return          la representación escrita con letras
+         */
+        static String convertir(int digito) {
+            //Si el número es mayor a 9 o inferior a 0, hubo un error
+            if (digito < 0 || digito > 9) {
+                System.out.println("No se pueden convertir decenas: Digito fuera de rango.");
+                return "-";
+            }
+
+            //Busca el valor que corresponda
+            for (Decenas decena : Decenas.values()) {
+                if (digito == decena.valor) {
+                    return decena.toString();
+                }
+            }
+
+            //Esta última rama nunca debería de suceder
+            System.out.println("No se pueden convertir decenas: No se encontro correspondencia.");
+            return "~";
+        }
+
+        @Override
+        public String toString() {
+            if (this.valor == 0) {
+                return "";
+            }
+            return super.toString();
+        }
+    }
+
     private enum Unidades {
         Cero(0),
         Uno(1),
@@ -24,7 +75,7 @@ public class CantidadesToLetras {
          * @param digito    representa una cantidad de unidades
          * @return          la representación escrita con letras
          */
-        static String convertirUnidades(int digito) {
+        static String convertir(int digito) {
             //Si el número es mayor a 9 o inferior a 0, hubo un error
             if (digito < 0 || digito > 9) {
                 System.out.println("No se pueden convertir unidades: Digito fuera de rango.");
@@ -34,7 +85,7 @@ public class CantidadesToLetras {
             //Busca el valor que corresponda
             for (Unidades unidad : Unidades.values()) {
                 if (digito == unidad.valor) {
-                    return unidad.name();
+                    return unidad.toString();
                 }
             }
 
@@ -42,7 +93,16 @@ public class CantidadesToLetras {
             System.out.println("No se pueden convertir unidades: No se encontro correspondencia.");
             return "~";
         }
+
+        @Override
+        public String toString() {
+            if (this.valor == 0) {
+                return "";
+            }
+            return super.toString();
+        }
     }
+
     /**
      * Convierte un número entero a letras, por ejemplo 319 a "Trescientos Diecinueve" usando un algoritmo recursivo.
      * @param numero    el número a convertir
@@ -67,31 +127,41 @@ public class CantidadesToLetras {
             int decenas = digitoEvaluado / 10;
 
             //TODO: hacer más elegante: módulo y división entre 10, si está entre 11 y 15 o 21 y 29, caso especial, si no, construir normalmente
-            if (decenas == 0 || decenas >= 3) {
-                importeEnLetras.append(Unidades.convertirUnidades(unidades));
+            if (unidades == 0) {
+                importeEnLetras.append(Decenas.convertir(decenas));
+            } else if (decenas == 0 || decenas >= 3) {
+                String y = "";
+                if (decenas != 0) {
+                    y = " y ";
+                }
+                importeEnLetras.append(Decenas.convertir(decenas)).append(y).append(Unidades.convertir(unidades)).append(" ");
             } else {
-                importeEnLetras.append("hola que hace");
+                if (digitoEvaluado <= 15) {
+                    switch (unidades) {
+                        case 1:
+                            importeEnLetras.append("Once ");
+                            break;
+                        case 2:
+                            importeEnLetras.append("Doce ");
+                            break;
+                        case 3:
+                            importeEnLetras.append("Trece ");
+                            break;
+                        case 4:
+                            importeEnLetras.append("Catorce ");
+                            break;
+                        case 5:
+                            importeEnLetras.append("Quince ");
+                            break;
+                    }
+                } else if (decenas == 1) {
+                    importeEnLetras.append("Dieci").append(Unidades.convertir(unidades).toLowerCase()).append(" ");
+                } else if (decenas == 2) {
+                    importeEnLetras.append("Veinti").append(Unidades.convertir(unidades).toLowerCase()).append(" ");
+
+                }
             }
 
-                /*
-            } else if (digitoEvaluado == 10) {
-
-
-            } else if (digitoEvaluado < 16) {
-
-
-            } else if (digitoEvaluado < 20) {
-
-
-            } else if (digitoEvaluado == 20) {
-                // Veinte es diferente de las demás decenas porque no se dice "veinte y cinco" por ejemplo
-
-            } else if (digitoEvaluado < 30) {
-
-            } else {
-
-            }
-            */
         } else {                                        //Cuando el orden de magnitud NO ES múltiplo de 3
             digitoEvaluado = numero % 10;
             digitoSiguiente = numero / 10;
--- src/com/auebo/utils/cantidadestoletras/Tester.java
+++ src/com/auebo/utils/cantidadestoletras/Tester.java
@@ -2,7 +2,7 @@ package com.auebo.utils.cantidadestoletras;
 
 public class Tester {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 100; i++) {
             System.out.printf("%d: %s\n", i, CantidadesToLetras.convertirImporte(i,0) );
         }
     }
